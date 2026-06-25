df3c06857a793f1e52fecedb1da1bd872ada84d6
tjdwlckd-ui <tjdwlckd@gmail.com>
2026-06-25 20:18:04 +0900

    Delete js directory

--- js/script.js
+++
@@ -1,5 +0,0 @@
-import { db } from './firebase-config.js';
-
-document.getElementById('cta-btn').addEventListener('click', () => {
-  document.getElementById('about').scrollIntoView({ behavior: 'smooth' });
-});
